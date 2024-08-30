46e10f695d7976a12fb0cd7c7b005bb5c0a8864f
F. Ardan Dal Rì <43485095+return-nihil@users.noreply.github.com>
2024-08-30 11:26:11 +0200

    Add files via upload

--- train/fdbk_dataset_class.py
+++ train/fdbk_dataset_class.py
@@ -17,20 +17,21 @@ class FDBK_Dataset(Dataset):
 
 
         # Essentia algorithm instances defined here to save memory
-        self.w = es.Windowing(type='hann', size=self.block_size)
+        self.w = es.Windowing(type='hann')
         self.spectrum = es.Spectrum(size=self.block_size)
         self.spectral_peaks = es.SpectralPeaks(maxPeaks=10, minFrequency=30, sampleRate=self.sample_rate) #https://essentia.upf.edu/reference/std_SpectralPeaks.html
-        self.pitch_detect = es.PitchYinFFT(sampleRate=self.sample_rate, frameSize=self.block_size)
-        self.mfcc = es.MFCC(numberCoefficients=NUM_MFCCS, inputSize=int(self.block_size/2)+1)
-        self.spectral_contrast = es.SpectralContrast(sampleRate=self.sample_rate, frameSize=self.block_size)
+        self.pitch_detect = es.PitchYinFFT()
+        self.mfcc = es.MFCC(numberCoefficients=NUM_MFCCS)
+        self.melbands = es.MelBands(numberBands=40, lowFrequencyBound=50, highFrequencyBound=8000)
+        self.spectral_contrast = es.SpectralContrast()
         self.inharmonicity = es.Inharmonicity()
         self.dissonance = es.Dissonance()
-        self.pitch_salience = es.PitchSalience(sampleRate=self.sample_rate)
+        self.pitch_salience = es.PitchSalience()
         self.flatness = es.Flatness()
 
 
     def _load_audio(self, path):
-        y, sr = librosa.load(path, sr=self.sample_rate, dtype=np.float32)
+        y, sr = librosa.load(path, sr=self.sample_rate)
         return y, sr
 
     def _extract_features(self, y):
@@ -42,21 +43,22 @@ class FDBK_Dataset(Dataset):
         elif self.mode == 'feature':
             freqs, mags = self.spectral_peaks(spectrum)
             _, mfcc = self.mfcc(spectrum)
+            melbands_spectrum = self.melbands(spectrum)
             spectral_contrast, _ = self.spectral_contrast(spectrum)
             inharmonicity = self.inharmonicity(freqs, mags)
             dissonance = self.dissonance(freqs, mags)
             pitch_salience = self.pitch_salience(spectrum)
             flatness = self.flatness(spectrum)
-            return np.concatenate((mfcc, spectral_contrast, [inharmonicity, dissonance, pitch_salience, flatness]))
+            return np.concatenate((mfcc, melbands_spectrum, spectral_contrast, [inharmonicity, dissonance, pitch_salience, flatness]), dtype=np.float32)
         else:
             raise ValueError(f"mode '{self.mode}' not recognised, please use one of the following: ['mfcc', 'feature']")
         return features
 
     def _resize_if_necessary(self, audio, shape):
         if audio.size < shape:
-            print("found short sample in dataset, only a problem if frequent")
-            zeros = np.zeros((shape), dtype=np.float32)
+            zeros = np.zeros((shape))
             zeros[:audio.size] += audio
+            # audio = np.pad(audio, ((0, 0), (shape - audio.size)), mode='constant')
             audio = zeros
         else:
             audio = audio[:shape]
@@ -70,5 +72,8 @@ class FDBK_Dataset(Dataset):
         audio = self._resize_if_necessary(audio, self.block_size)
         features = np.array(self._extract_features(audio), dtype=np.float32)
         synth_parameters = np.array(self.dataframe.iloc[idx].values[1:], dtype=np.float32)
+        #print(f"features: {features.shape}, synth_parameters: {synth_parameters.shape}")
 
         return features, synth_parameters
+
+
